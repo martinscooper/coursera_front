143c30d077a50d240a07c0aa2e26f685582d5fb4
Martín Santillán Cooper <marsancoo@gmail.com>
2020-09-27 18:20:34 +0200

    Added json-server folder

---
+++ json-server/public/precache-manifest.e625b95627525cef3d210032fb88ea41.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21633c4aa2b0c021882e89d93fc225ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b922555b91069591ac4",
+    "url": "/static/css/2.b0e1eb24.chunk.css"
+  },
+  {
+    "revision": "c918187251f989e0a5a0",
+    "url": "/static/css/main.0a8336c2.chunk.css"
+  },
+  {
+    "revision": "7b922555b91069591ac4",
+    "url": "/static/js/2.81a8dc1d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.81a8dc1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c918187251f989e0a5a0",
+    "url": "/static/js/main.1958178a.chunk.js"
+  },
+  {
+    "revision": "aff20233fc33123ab008",
+    "url": "/static/js/runtime-main.74f98a6f.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
